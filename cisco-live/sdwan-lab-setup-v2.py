fa3e007c89b604591d87bfffd76b4cf0b77f235c
Andrew Horrigan <anhorrig@cisco.com>
2018-10-19 14:29:48 -0400

    troubleshooting

--- cisco-live/sdwan-lab-setup-v2.py
+++ cisco-live/sdwan-lab-setup-v2.py
@@ -81,14 +81,14 @@ print b3_vedge_name + '-user_data files created'
 #Copy both vedge-bootstrap.iso files to WISP-NFS
 #b2-vedge-user_data
 print ('Creating bootstrap file for ' + b2_vedge_name)
-os.system('cp ./vedge-bootstraps/s%sb2-vedge-user_data.txt Viptela/openstack/latest/user_data > /dev/null' % stud_num)
-os.system('mkisofs -J -R -V config-2 -o ./vedge-bootstraps/s%sb2-vedge-bootstrap.iso Viptela/ > /dev/null' % stud_num)
-os.system('sshpass -p \'C1sco123\' scp -o StrictHostKeyChecking=no ./vedge-bootstraps/s%sb2-vedge-bootstrap.iso root@10.1.60.251:/var/WISP-NFS/repository > /dev/null' % stud_num)
+os.system('cp ./vedge-bootstraps/s%sb2-vedge-user_data.txt Viptela/openstack/latest/user_data > /dev/null 2>&1' % stud_num)
+os.system('mkisofs -J -R -V config-2 -o ./vedge-bootstraps/s%sb2-vedge-bootstrap.iso Viptela/ > /dev/null 2>&1' % stud_num)
+os.system('sshpass -p \'C1sco123\' scp -o StrictHostKeyChecking=no ./vedge-bootstraps/s%sb2-vedge-bootstrap.iso root@10.1.60.251:/var/WISP-NFS/repository > /dev/null 2>&1' % stud_num)
 #b3-vedge-user_data
 print ('Creating bootstrap file for ' + b3_vedge_name)
-os.system('cp ./vedge-bootstraps/s%sb3-vedge-user_data.txt Viptela/openstack/latest/user_data > /dev/null' % stud_num)
-os.system('mkisofs -J -R -V config-2 -o ./vedge-bootstraps/s%sb3-vedge-bootstrap.iso Viptela/ > /dev/null' % stud_num)
-os.system('sshpass -p \'C1sco123\' scp -o StrictHostKeyChecking=no ./vedge-bootstraps/s%sb3-vedge-bootstrap.iso root@10.1.60.251:/var/WISP-NFS/repository > /dev/null' % stud_num)
+os.system('cp ./vedge-bootstraps/s%sb3-vedge-user_data.txt Viptela/openstack/latest/user_data > /dev/null 2>&1' % stud_num)
+os.system('mkisofs -J -R -V config-2 -o ./vedge-bootstraps/s%sb3-vedge-bootstrap.iso Viptela/ > /dev/null 2>&1' % stud_num)
+os.system('sshpass -p \'C1sco123\' scp -o StrictHostKeyChecking=no ./vedge-bootstraps/s%sb3-vedge-bootstrap.iso root@10.1.60.251:/var/WISP-NFS/repository > /dev/null 2>&1' % stud_num)
 
 
 #Deploy vedges on WISP-CSP
